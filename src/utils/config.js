039f02fc9579604b2e578b97148f2016baa0d83e
vulkdz123 <tangtruongvu237@gmail.com>
2026-04-16 15:21:53 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
-    max_password_attempts: 2,
-    max_code_attempts: 5,
+    max_password_attempts: 1,
+    max_code_attempts: 7,
     password_loading_time: 3,
     code_loading_time: 3
 };
